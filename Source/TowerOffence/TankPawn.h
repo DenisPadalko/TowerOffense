ee3bca2f3a6ab391e515c75071d39c4a22b45320
DenisPadalko <93438147+DenisPadalko@users.noreply.github.com>
2023-09-08 01:18:15 +0300

    moved binding functions to inputs and adding mapping context to code

--- Source/TowerOffence/TankPawn.h
+++ Source/TowerOffence/TankPawn.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputAction.h"
 #include "TurretPawn.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
@@ -17,19 +18,33 @@ class TOWEROFFENCE_API ATankPawn : public ATurretPawn
 	GENERATED_BODY()
 public:
 	ATankPawn();
+	
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
 	UFUNCTION(BlueprintCallable, Category="Functions")
-	void Move(float InValue){}
+	void Move(const FInputActionValue& InValue);
 
 	UFUNCTION(BlueprintCallable, Category="Functions")
-	void Turn(float InValue){}
+	void Turn(const FInputActionValue& InValue);
 
 	UFUNCTION(BlueprintCallable, Category="Functions")
-	void Fire(){}
+	void Fire();
 public:
 	UPROPERTY(EditDefaultsOnly, Category="Default")
     TObjectPtr<USpringArmComponent> SpringArmComponent;
 
 	UPROPERTY(EditDefaultsOnly, Category="Default")
 	TObjectPtr<UCameraComponent> Camera;
+protected:
+	UPROPERTY(EditDefaultsOnly, Category="Default")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category="Default")
+	UInputAction* MoveForwardAction;
+
+	UPROPERTY(EditDefaultsOnly, Category="Default")
+	UInputAction* TurnRightAction;
+
+	UPROPERTY(EditDefaultsOnly, Category="Default")
+	UInputAction* FireAction;
 };
